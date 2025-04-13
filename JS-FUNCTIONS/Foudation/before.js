32c1210c18397bc2a53e8ca5223ea6e6f1798d22
Jesualdo Brandão Dias Júnior <jesualdo.dias@fatec.sp.gov.br>
2025-04-13 16:50:32 -0300

    Meu primeiro commit

---
+++ JS-FUNCTIONS/Foudation/before.js
@@ -0,0 +1,17 @@
+//1. Prerequisites
+
+// -> Basic Knowledge of JS
+// ---> Data types: number, string, boolean, array and object
+// ---> Operators (+, -, /, &&, ||, >=, <=,...)
+// ---> If, For, While
+
+// 2. Node
+// -> NodeJS (v14.6.0)
+
+// 3. Visual Studio code
+// -> Code Runner
+// --> Code-runner: Clear Previous Output
+// --> Ctrl + Alt + n (run code)
+// --> Ctrl + Alt + m (stop the running code)
+
+console.log("test version 2!")
